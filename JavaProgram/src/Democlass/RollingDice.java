067a1763a5246f454f47c8f539900ec0c7b42885
Sumalatha.thummetti <thummettisuma@gmail.com>
2022-02-10 09:42:07 +1100

    sample repo

---
+++ JavaProgram/src/Democlass/RollingDice.java
@@ -0,0 +1,13 @@
+package Democlass;
+
+import java.util.Random;
+
+public class RollingDice {
+
+    public static void main(String[] args){
+        Random coolNumber = new Random();
+        int x =  coolNumber.nextInt(6)+1;
+        System.out.println("You rolled a :" + x);
+    }
+
+}
